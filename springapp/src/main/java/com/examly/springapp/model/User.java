a5ef53ce4506087643bd432bfd414dc6dde32eee
ajaydr16 <ajaydr16@gmail.com>
2021-10-02 14:09:26 +0000

    added user model

---
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -0,0 +1,29 @@
+package main.java.com.examly.springapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    String email;
+
+    String userName;
+
+    String password;
+
+    String mobile;
+
+    int age;
+
+    String userRole;
+}
