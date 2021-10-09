ea3d86911c7a3816d1a1281742f0c7305bf460e9
ajaydr16 <ajaydr16@gmail.com>
2021-10-09 15:04:28 +0000

    resolved errors in bookingModel

--- springapp/src/main/java/com/examly/springapp/model/Booking.java
+++ springapp/src/main/java/com/examly/springapp/model/Booking.java
@@ -1,7 +1,6 @@
 package com.examly.springapp.model;
 
 import javax.persistence.Entity;
-import com.examly.springapp.model.Room;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.Getter;
@@ -9,7 +8,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Entity(name="Booking")
 public class Booking {
 
@@ -30,14 +28,5 @@ public class Booking {
     String hotelAddress;
 
     int adminId;
-    public Booking(int userId, String price, int roomId, String hotelName, String hotelImageURL, String hotelAddress, int adminId) {
-        this.userId = userId;
-        this.price = price;
-        this.roomId = roomId;
-        this.hotelName = hotelName;
-        this.hotelImageURL = hotelImageURL;
-        this.hotelAddress = hotelAddress;
-        this.adminId = adminId;
-    }
 
 }
