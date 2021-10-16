8d3cb540267464b3154a19cce8560d06a1c72226
ajaydr16 <ajaydr16@gmail.com>
2021-10-16 11:01:58 +0000

    added fetch example

--- reactapp/src/UserHotelBooking.js
+++ reactapp/src/UserHotelBooking.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import './UserHotelBooking.css';
 function userHotelBooking(){
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    componentDidMount() {
+
+        fetch('<API URL>')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
     
     const bookingList=[
         {
@@ -26,8 +53,10 @@ function userHotelBooking(){
         }
     ];
 
+    const { isLoaded, items } = this.state;
 
     return(
+        
         <div className="userBookingBody">
            <table className="ta" align="center">
                <tr className="tr1" cellSpacing="50">
