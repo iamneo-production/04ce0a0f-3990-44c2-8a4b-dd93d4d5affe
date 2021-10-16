5ced4615d2e4ba704c5199c989fa335845541652
ajaydr16 <ajaydr16@gmail.com>
2021-10-16 07:23:49 +0000

    routings

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
-import Profile from "./Profile";
+import UserHotelBooking from './UserHotelBooking';
+import reportWebVitals from './reportWebVitals';
+import Editprofile from './EditProfile';
+import AdminProfileEdit from './AdminProfileEdit';
+import AdminBooking from './AdminBooking';
+import AdminEditRoom from './AdminEditRoom';
 
 function App() {
   return (
@@ -9,12 +14,67 @@ function App() {
     <div className="app">
       
       <Switch>
+      {/* User Routings   */}
+      <Route path="/user/signup">          
+         
+      </Route>  
       <Route path="/login">          
          
-      </Route>   
-      <Route path="/checkout">   
-          <Profile />
-        </Route>
+      </Route>
+      <Route path="/user/dashboard">          
+         
+      </Route> 
+      <Route path="/user/hotelDetail/{hotelId}">          
+         
+      </Route>
+      <Route path="/user/roomDetail/{roomId}">          
+         
+      </Route>
+      <Route path="/user/editProfile/{userId}">          
+         
+      </Route>
+      <Route path="/user/bookings/{userId}">          
+         
+      </Route>
+      <Route path="/user/profile/{userId}">          
+         
+      </Route>
+
+      {/* Admin Routings */}
+
+      <Route path="/admin/signup">          
+         
+      </Route> 
+      <Route path="/admin/profile/{adminId}">          
+         
+      </Route>
+      <Route path="/admin/dashboard">          
+         
+      </Route>
+      <Route path="/admin/addRoom">          
+         
+      </Route>
+      <Route path="/admin/editRoom/{roomId}">          
+         
+      </Route>
+      <Route path="/admin/bookings/{adminId}">          
+         
+      </Route>
+      <Route path="/admin/editProfile/{adminId}">          
+         
+      </Route>
+
+
+      {/* SuperAdmin Routings */}
+      <Route path="/superadmin/login">          
+         
+      </Route>
+      <Route path="/superadmin/adminList">          
+         
+      </Route>
+      <Route path="/superadmin/adminBookings">          
+         
+      </Route>
       </Switch>
     </div>
     </Router>
