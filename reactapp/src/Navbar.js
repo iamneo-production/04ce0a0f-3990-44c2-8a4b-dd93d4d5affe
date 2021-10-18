299b58cbb25c0f056c223068c2fb40c7247adcdc
ajaydr16 <ajaydr16@gmail.com>
2021-10-18 16:13:38 +0000

    user navbar

---
+++ reactapp/src/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import  { Redirect } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { Navbar,Nav} from 'react-bootstrap';
+import Editprofile from './EditProfile';
+import './UserNavbar.css';
+import UserProfleLink from './UserDashboard';
+import UserBookingLink from './UserHotelBooking';
+import UserDashboardLink from './UserDashboard';
+import { useHistory } from 'react-router-dom';
+
+function NavbarHome(){
+        const history = useHistory();
+        function logout(){
+            localStorage.clear();
+            sessionStorage.clear();
+            history.push("/superadmin/login");
+        }
+        return (
+            // <Router>
+                <div className="Navbar_Hotel">
+                 <a href='#' className="title"><strong>Rental Rooms</strong></a>
+                                {/* <Link to="/user/dashboard" className="dashboard">Dashboard</Link>
+                                <Link to="/user/profile" className="profile">Profile</Link>
+                                <Link to="/user/booking" className="mybooking">MyBooking</Link> */}
+                                
+                                <button onClick={()=>{logout()}} className="UserLogoutButton">Super Admin</button>
+                            <br/>
+   
+        
+                    
+                </div>
+        )
+        }
+export default NavbarHome;
