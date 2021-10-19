a6a46f494cd709cccf605396f73e2cf6c4c15373
ajaydr16 <ajaydr16@gmail.com>
2021-10-19 06:14:42 +0000

    AdminProfile

---
+++ reactapp/src/AdminProfile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './AdminProfile.css';
+function AdminProfile(){
+    return(
+        <div class="x">
+        
+        <div class="main">
+        <div class="sub1">
+           <p>Name : </p>
+           <p>Email : </p>
+           <p>Mobile no :</p>
+           <p>password : </p>
+        </div>
+        <div class="sub2">
+          <p>Hotel Name : </p>
+          <p>Hotel Address: </p>  
+        </div>
+        </div>
+        <div class="editprofile">
+        <input type="image" src="profilepic.png"/>
+        <br/>
+        <button>edit profile</button>
+        </div>
+     </div>
+    );
+
+}
+
+export default AdminProfile;
