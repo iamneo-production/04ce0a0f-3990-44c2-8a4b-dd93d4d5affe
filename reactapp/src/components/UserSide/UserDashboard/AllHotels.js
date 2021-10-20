97ba2479a22df0374ba597ba8dd69c324f799b02
ajaydr16 <ajaydr16@gmail.com>
2021-10-20 15:46:19 +0000

    address

--- reactapp/src/components/UserSide/UserDashboard/AllHotels.js
+++ reactapp/src/components/UserSide/UserDashboard/AllHotels.js
@@ -31,7 +31,7 @@ function AllHotels(props) {
                 <Grid container spacing={1}>
                     <Grid item xs={4} sm={4} className="inner-div">
                     <input width="50px" type="image" src="https://cdn-icons.flaticon.com/png/512/3177/premium/3177440.png?token=exp=1634710666~hmac=38856b65b805d361e8894e2111b60e6c"/>
-                        <p style={{marginLeft:"5%"}}>Address: {props.hotelAddress}</p>
+                        <p style={{marginLeft:"5%"}}>{props.hotelAddress}</p>
                     </Grid>
                     <Grid item xs={4} sm={4}>
                         <p>Hotel Name: {props.hotelName}</p>
